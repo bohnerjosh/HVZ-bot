6542b3e09112eab823159d8dcedc763c34e068fa
bohnerjosh <Squash0002@gmail.com>
2022-05-04 14:38:08 -0500

    added database init code to bot

--- hvz.py
+++ hvz.py
@@ -1,8 +1,14 @@
 import os
 import discord
+from database import Database
+from pathlib import Path
 
+DATABASE_NAME = "db"
 TOKEN = os.environ["DISCORD_TOKEN"]
 client = discord.Client()
+
+parent_dir = Path( __file__ ).parent.resolve()
+db = Database(str(parent_dir / DATABASE_NAME))
 
 @client.event
 async def on_ready():
